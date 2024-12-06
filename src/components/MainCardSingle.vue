2ac4f144421692694dd2db00beb8f70d8354647a
Daniel Garza <dgarz160@stu.southtexascollege.edu>
2024-12-05 19:13:39 -0600

    fixed size of boxes

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -35,7 +35,8 @@ const props = defineProps({
     <!-- {{ fullname }} <br /> -->
       <RouterLink :to="{name: 'CardDetails', params: {id: props.employee._id}}">
         <div class="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
-                <img :src="faker.image.urlLoremFlickr({category: 'cat'})" class="rounded-t-xl" />
+                <!-- <img :src="faker.image.urlLoremFlickr({category: 'cat'})" class="rounded-t-xl" /> -->
+                 <img :src="faker.image.urlLoremFlickr({category: 'cat', width: 640, height: 480})" class="rounded-t-xl" />
                 <div class="p-4">
                     <p class="mb-2 text-2xl font-bold">{{ props.employee.firstName }} {{ props.employee.lastName }}</p>
                     <p class="mb-3 font-normal text-gray-700">{{ props.employee.title }}</p>
